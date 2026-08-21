b264d6974fcfca3affee9f34839744df7343123c
jasonm23 <jasonm23@gmail.com>
2026-08-21 07:48:21 +0700

    hot load completion (don't cache completion file)

--- zsh-patina.bundle.zsh
+++ zsh-patina.bundle.zsh
@@ -2,34 +2,15 @@
 #
 # Zsh integration bundle for zsh-patina
 # https://github.com/michel-kraemer/zsh-patina
+if (( $+commands[zsh-patina] )); then
+    eval "$(zsh-patina activate)"
 
-if (( ! $+commands[zsh-patina] )); then
-    print -u2 "zsh-patina: command not found"
-    print -u2 "Install zsh-patina using your preferred package manager."
-    print -u2 "See https://github.com/michel-kraemer/zsh-patina for installation options."
-    return 1
+    # zsh-patina completion support
+    if (( ! $+functions[_zsh-patina] )); then
+	eval "$(zsh-patina completion)"
+    fi
 fi
 
-eval "$(zsh-patina activate)"
-
-# zsh-patina completion support
-#
-# Generated completion is kept separate from bundle-managed files.
-# compinit/compaudit remain the responsibility of the bundle manager/user.
-
-local completion_dir=${ZDOTDIR:-~}"/.cache/zsh-patina/completions"
-local completion_file="$completion_dir/_zsh-patina"
-
-if [[ ! -r "$completion_file" ]]; then
-    mkdir -p "$completion_dir"
-    chmod go-w "$completion_dir"
-
-    zsh-patina completion --output-file "$completion_file" || {
-        print -u2 "zsh-patina: failed to generate completion"
-        return 1
-    }
-
-    chmod go-w "$completion_file"
-fi
-
-fpath=("$completion_dir" $fpath)
+print -u2 "zsh-patina: command not found"
+print -u2 "Install zsh-patina using your preferred package manager."
+print -u2 "See https://github.com/michel-kraemer/zsh-patina for installation options."
